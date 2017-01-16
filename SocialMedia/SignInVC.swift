389f2dbd6aa237b2155c1894aead91fac9676b30
Jefferson Setiawan <jefferson.setiawan@mirumagency.com>
2017-01-16 17:54:02 +0700

    First commit

---
+++ SocialMedia/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Jefferson Setiawan on 1/16/17.
+//  Copyright © 2017 Jefferson Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
